6ba7de37db9edee7f969cbc2d98f0f4625eea159
ezzye <ezzy.elliott@gmail.com>
2021-06-23 17:27:34 +0100

    Innovation-day: Hello world

--- src/main/java/com/myorg/InnovationDayAwsCdkHelloWorldStack.java
+++ src/main/java/com/myorg/InnovationDayAwsCdkHelloWorldStack.java
@@ -3,6 +3,7 @@ package com.myorg;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.s3.Bucket;
 
 public class InnovationDayAwsCdkHelloWorldStack extends Stack {
     public InnovationDayAwsCdkHelloWorldStack(final Construct scope, final String id) {
@@ -13,5 +14,7 @@ public class InnovationDayAwsCdkHelloWorldStack extends Stack {
         super(scope, id, props);
 
         // The code that defines your stack goes here
+        Bucket.Builder.create(this, "innovationDayBucket")
+                .versioned(true).build();
     }
 }
